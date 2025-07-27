9f58b54aab0da694b6259364d4c310bd513e96fc
pranaysultania <pranaysultania6@gmail.com>
2025-07-27 19:32:08 +0530

    Fix CORS for frontend authentication

--- backend/server.py
+++ backend/server.py
@@ -35,7 +35,10 @@ api_router = APIRouter(prefix="/api")
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://pranay-portfolio-v3.vercel.app",
+        "http://localhost:3000"  # For local development
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
